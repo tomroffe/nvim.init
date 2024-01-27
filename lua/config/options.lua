5faff29c7e26002c08a3627049f46e6e0ceb74ed
Tom Roffe <tom.roffe@gmail.com>
2024-01-27 20:43:29 +0000

    Inital commit of my developing Neovim config.
    Signed-off-by: Tom Roffe <tom.roffe@gmail.com>

---
+++ lua/config/options.lua
@@ -0,0 +1,22 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+vim.opt.guicursor = ""
+
+vim.g.python3_host_prog = "/Users/tom/.pyenv/versions/neovim/bin/python"
+vim.g.loaded_perl_provider = 0
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
