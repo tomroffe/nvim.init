4941ad3dd616ef5070b97acb58b7ded6a9cc7374
Tom Roffe <tom.roffe@gmail.com>
2024-02-04 22:26:47 +0000

    Add telescope config with fzf config

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,31 @@
+return {
+	{
+		"nvim-telescope/telescope.nvim",
+		-- add telescope-fzf-native
+		dependencies = {
+			"nvim-telescope/telescope-fzf-native.nvim",
+			build = "make",
+			config = function()
+				require("telescope").load_extension("fzf")
+			end,
+		},
+		keys = {
+      -- add a keymap to browse plugin files
+      -- stylua: ignore
+      {
+        "<leader>fp",
+        function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
+        desc = "Find Plugin File",
+      },
+		},
+		-- change some options
+		opts = {
+			defaults = {
+				layout_strategy = "horizontal",
+				layout_config = { prompt_position = "top" },
+				sorting_strategy = "ascending",
+				winblend = 0,
+			},
+		},
+	},
+}
