85ba112d2a83318a56b4c7ae16a59002c8fa5d89
Tom Roffe <tom.roffe@gmail.com>
2024-02-09 21:26:03 +0000

    Update lazy module versions, add telescope undo
    - update spellcheck list
    - removed undotree

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,6 +3,9 @@ return {
 		"nvim-telescope/telescope.nvim",
 		-- add telescope-fzf-native
 		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"debugloop/telescope-undo.nvim",
+
 			"nvim-telescope/telescope-fzf-native.nvim",
 			build = "make",
 			config = function()
@@ -10,13 +13,18 @@ return {
 			end,
 		},
 		keys = {
-      -- add a keymap to browse plugin files
-      -- stylua: ignore
-      {
-        "<leader>fp",
-        function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
-        desc = "Find Plugin File",
-      },
+			{
+				"<leader>fp",
+				function()
+					require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root })
+				end,
+				desc = "Find Plugin File",
+			},
+			{
+				"<leader>U",
+				"<cmd>Telescope undo<cr>",
+				desc = "Telescope Undo",
+			},
 		},
 		-- change some options
 		opts = {
